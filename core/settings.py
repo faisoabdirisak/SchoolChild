410a86500ceb9c82ab780c5050f6cf78177a76f0
faiso abdirisak <faisoabdirisak@gmail.com>
2022-06-21 21:05:06 +0300

    add

--- core/settings.py
+++ core/settings.py
@@ -14,9 +14,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=s8#hze52)sw&a^9t_r5k$le_yxipy3m5s(j-ao19(1p(a2g)_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fa-sa.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -35,6 +35,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
